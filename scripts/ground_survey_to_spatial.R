ddbf87729d392bb78b8bdbd68811cb886368f211
Unknown <derek.j.n.young@gmail.com>
2020-01-04 14:17:36 -0800

    Script to convert ground-truth survey data into GeoJSON

---
+++ scripts/ground_survey_to_spatial.R
@@ -0,0 +1,113 @@
+#### Convert Weeks' Emerald Point ground-truth survey data into GeoJSON
+# Author: Derek Young
+
+data_dir = "C:/Users/DYoung/Box/projects/forestuav_data/"
+
+library(tidyverse)
+library(readxl)
+library(sf)
+
+#### Conveinence functions ####
+deg_to_rad = function(x) {
+  return(x*pi/180)
+}
+
+
+#### Load and clean data ####
+
+trees = read_excel(paste0(data_dir,"/ground_truth/emerald_point/field/EPT_tree_data.xlsx"),sheet=1)
+plots = read_excel(paste0(data_dir,"/ground_truth/emerald_point/field/EPT_tree_data.xlsx"),sheet=2)
+
+trees = trees %>%
+  mutate(Distance = as.numeric(Distance))
+
+
+
+
+#### For each plot, get coords of each data collection location ####
+
+plots_locs = data.frame()
+
+for(i in 1:nrow(plots)) {
+  
+  plot = plots[i,]  
+  trees_plot = trees %>%
+    filter(Plot == plot$Plot)
+  
+  locs = unique(trees_plot$data_col_location)
+  
+  for(loc in locs) {
+
+    
+    if(loc == 1) {
+      
+      if(plot$Data_collection_location_1 != "plot center") {
+        cat("Data collection location 1 is not plot center for plot",plot$Plot,"\n")
+      }
+      
+      loc_x = plot$Easting
+      loc_y = plot$Northing
+      
+      plot_loc = data.frame(Plot = plot$Plot,
+                            loc = loc,
+                            col_loc_easting = loc_x,
+                            col_loc_northing = loc_y)
+      
+      plots_locs = rbind(plots_locs,plot_loc)
+      
+    } else {
+      
+      loc_distance_column_name = paste0("Data_collection_location_",loc,"_distance_from plot center_m")
+      loc_distance_column_number = which(names(plot) == loc_distance_column_name)
+      if(is.null(loc_distance_column_number)) {
+        cat("No column properly named for distance for plot: ",plot$Plot,", location: ",loc)
+      }
+      
+      loc_azimuth_column_name = paste0("Data_collection_location_ ",loc,"_Azimuth_from_plot_center(corrected for declination)")
+      loc_azimuth_column_number = which(names(plot) == loc_azimuth_column_name)
+      if(is.null(loc_azimuth_column_number)) {
+        cat("No column properly named for azimuth for plot: ",plot$Plot,", location: ",loc)
+      }
+      
+      loc_azimuth = plot[,loc_azimuth_column_number]
+      loc_distance = plot[,loc_distance_column_number]
+      
+      loc_x = plot$Easting + sin(deg_to_rad(loc_azimuth)) * loc_distance
+      loc_y = plot$Northing + cos(deg_to_rad(loc_azimuth)) * loc_distance
+      
+      plot_loc = data.frame(Plot = plot$Plot,
+                            loc = loc,
+                            col_loc_easting = loc_x %>% as.numeric,
+                            col_loc_northing = loc_y %>% as.numeric) 
+      
+      plots_locs = rbind(plots_locs,plot_loc)
+    }
+  }
+}
+
+
+
+
+#### Pull in the plot data colleciton location info for each tree to simplify the mapping step ####
+trees_col_locs = left_join(trees,plots_locs,by=c("Plot"="Plot","data_col_location"="loc"))
+
+
+
+#### Get the coords of each tree ####
+trees_locs = trees_col_locs %>%
+  mutate(Easting = col_loc_easting + sin(deg_to_rad(Azimuth_converted)) * Distance,
+         Northing = col_loc_northing + cos(deg_to_rad(Azimuth_converted)) * Distance)
+  
+
+#### Convert to spatial ####
+
+# need to drop trees without coordinates (they exist because of incomplete survey data)
+trees_locs = trees_locs %>%
+  filter(!is.na(Easting))
+
+
+trees_sp <- st_as_sf(trees_locs, coords = c("Easting","Northing"), crs = 32610)
+
+st_write(trees_sp %>% st_transform(4326),paste0(data_dir,"ground_truth/emerald_point/field/ept_trees.geojson"),delete_dsn=TRUE)
+
+         
