c0e73c8beb2b04bb9fd07d8c0a71251eb6b896bb
Derek <derek.j.n.young@gmail.com>
2020-04-02 18:50:27 -0700

    Summaries of shifted trees

--- scripts/check_stem_map_alignment.R
+++ scripts/check_stem_map_alignment.R
@@ -41,7 +41,7 @@ compare_ground_drone = function(i) {
 
   # filter drone trees to those within +- 25% height
   drone_trees_heightmatch = drone_trees %>%
-    filter(height %>% between(tree$Height-10, tree$Height+10))
+    filter(height %>% between(tree$Height-8, tree$Height+8))
   
   # get the closest of the height match trees
   drone_tree_match_index = st_nearest_feature(tree,drone_trees_heightmatch)
@@ -73,9 +73,58 @@ compare_ground_drone = function(i) {
   return(tree_match)
 }
 
-
-
 tree_matches = map_dfr(1:nrow(ground_trees_corr), compare_ground_drone)
+
+
+#### Compute derived match variables ####
+
+tree_matches = tree_matches %>%
+  mutate(shifted = ground_tree_shift_x > 0.01 | ground_tree_shift_y > 0.01) %>%
+  mutate(aligned = ground_drone_offset_x < 0.1 & ground_drone_offset_y < 0.1)
+
+
+#### Summarize shifted trees ####
+
+trees_shifted = tree_matches %>%
+  filter(shifted) %>%
+  group_by(ground_tree_plot,ground_tree_loc) %>%
+  summarize(n_trees_shifted = n(),
+            mean_x_shift = mean(ground_tree_shift_x),
+            mean_y_shift = mean(ground_tree_shift_y),
+            cv_x_shift = abs(sd(ground_tree_shift_x) / mean(abs(ground_tree_shift_x))),
+            cv_y_shift = abs(sd(ground_tree_shift_y) / mean(abs(ground_tree_shift_y))))
+
+## Pull in number of trees (aligned or not) per plot. This also has the effect of making sure all plot locs are included, even if no aligned trees
+
+all_subplots = tree_matches %>%
+  select(ground_tree_plot,ground_tree_loc) %>%
+  group_by(ground_tree_plot,ground_tree_loc) %>%
+  summarize(n_trees_total = n())
+
+tree_shift_summary = full_join(trees_shifted, all_subplots) %>%
+  mutate(n_trees_shifted = ifelse(is.na(n_trees_shifted),0,n_trees_shifted)) %>%
+  mutate(percent_trees_shifted = n_trees_shifted/n_trees_total * 100)
+
+## Make it look a little less overwhelming by rounding
+
+tree_shift_summary = tree_shift_summary %>%
+  mutate_at(vars(contains("shift")), function(x) round(x,3)) %>%
+  select(ground_tree_plot,ground_tree_loc,n_trees_shifted,n_trees_total,percent_trees_shifted,everything())
+
+
+#### Summarize by plot (not also subplot) ####
+
+tree_shift_summary_plot = tree_shift_summary %>%
+  group_by(ground_tree_plot) %>%
+  summarize(n_trees_shifted = sum(n_trees_shifted),
+            n_trees_total = sum(n_trees_total)) %>%
+  mutate(percent_trees_shifted = n_trees_shifted/n_trees_total)
+
+
+#### Write it ####
+
+write.csv(tree_shift_summary,data("alignment_eval/tree_shift_summary.csv"),row.names=FALSE)
+write.csv(tree_shift_summary_plot,data("alignment_eval/tree_shift_summary_plot.csv"),row.names=FALSE)
 
 
 
