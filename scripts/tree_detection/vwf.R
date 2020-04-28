6ca8f6d22e6dd191196e832fda12e2711a727d81
Derek <derek.j.n.young@gmail.com>
2020-04-28 14:39:49 -0700

    VWF for multiple CHMs at once

--- scripts/tree_detection/vwf.R
+++ scripts/tree_detection/vwf.R
@@ -20,58 +20,10 @@ source(here("scripts/convenience_functions.R"))
 
 #### Specify paramset of interest ####
 
-paramset_name = "paramset15a_02"
-
-
-
-#### Load data ####
-
-# find the dsm file in the metashape products direcotry
-chm_file = list.files(data("post_metashape_products/chm"),pattern=paramset_name, full.names=TRUE)
-if(length(chm_file) > 1) stop("More than 1 matching CHM file in the specified metashape data products folder.")
-if(length(chm_file) == 0) stop("No matching CHM files int he specified metashape data products folder.")
-
-chm = raster(chm_file)
-
-# create two smoothed rasters: 1 m window width and 0.5 m window width
-chm_res = res(chm) %>% mean
-pixels_smooth_1 = round(((0.5/chm_res)-1)/2)*2 + 1 # round to nearest odd integer
-pixels_smooth_2 = round(((1/chm_res)-1)/2)*2 + 1
-pixels_smooth_3 = round(((1.5/chm_res)-1)/2)*2 + 1
-
-weights = matrix(1,nrow=pixels_smooth_1,ncol=pixels_smooth_1)
-chm_smooth_1 = focal(chm, weights, fun=mean)
-
-weights = matrix(1,nrow=pixels_smooth_2,ncol=pixels_smooth_2)
-chm_smooth_2 = focal(chm, weights, fun=mean)
-
-weights = matrix(1,nrow=pixels_smooth_3,ncol=pixels_smooth_3)
-chm_smooth_3 = focal(chm, weights, fun=mean)
-
 #### Identify treetops ####
 #### and save to geojson
 
-### Define parameter values to search: only need to run if set defs change
 
-# b = seq(0.00, 0.06, by=0.01)
-# a = seq(0.0, 1, by = 0.1)
-# smooth = c(0,1,2,3)
-# 
-# params = expand.grid(a=a,b=b,smooth=smooth)
-# 
-# # remove sets where both a and b are zero
-# params = params %>%
-#   filter(!(a == 0 & b == 0))
-# 
-# params$detection_params_name = paste0("vwf_",str_pad(1:nrow(params), width=3, pad = "0"))
-# 
-# params$method = "vwf"
-# 
-# # save vwf params
-# write_csv(params,data("parameter_set_definitions/vwfdefs_fullrange.csv"))
-
-# load vwf params
-params = read_csv(data("parameter_set_definitions/vwfdefs_fullrange.csv"))
 
 
 
@@ -83,6 +35,8 @@ params = read_csv(data("parameter_set_definitions/vwfdefs_fullrange.csv"))
 
 vwf_singlechm_singleparamset = function(chm, chm_smooth_1, chm_smooth_2, chm_smooth_3, layer_name, a, b, smooth, detection_params_name) {
 
+  browser()
+  
   if(smooth == 1) {
     chm = chm_smooth_1
   } else if(smooth == 2) {
@@ -110,14 +64,98 @@ vwf_singlechm_singleparamset = function(chm, chm_smooth_1, chm_smooth_2, chm_smo
 }
 
 
-plan(multiprocess, workers = 6)
+vwf_singlechm_multiparamset = function(chm_layer_name, params = paramsets) {
 
-a = future_pmap(params %>% select(-method), vwf_singlechm_singleparamset , chm=chm, chm_smooth_1 = chm_smooth_1, chm_smooth_2 = chm_smooth_2, chm_smooth_3 = chm_smooth_3, layer_name = paramset_name)
+  #### Load data ####
+  
+  # find the dsm file in the metashape products direcotry
+  chm_file = list.files(data("post_metashape_products/chm"),pattern=chm_layer_name, full.names=TRUE)
+  if(length(chm_file) > 1) stop("More than 1 matching CHM file in the specified metashape data products folder.")
+  if(length(chm_file) == 0) stop("No matching CHM files int he specified metashape data products folder.")
+  
+  chm = raster(chm_file)
+  
+  ## Get the CHM layer name from the chm_file
+  
+  
+  # create two smoothed rasters: 1 m window width and 0.5 m window width
+  chm_res = res(chm) %>% mean
+  pixels_smooth_1 = round(((0.5/chm_res)-1)/2)*2 + 1 # round to nearest odd integer
+  pixels_smooth_2 = round(((1/chm_res)-1)/2)*2 + 1
+  pixels_smooth_3 = round(((1.5/chm_res)-1)/2)*2 + 1
+  
+  weights = matrix(1,nrow=pixels_smooth_1,ncol=pixels_smooth_1)
+  chm_smooth_1 = focal(chm, weights, fun=mean)
+  
+  weights = matrix(1,nrow=pixels_smooth_2,ncol=pixels_smooth_2)
+  chm_smooth_2 = focal(chm, weights, fun=mean)
+  
+  weights = matrix(1,nrow=pixels_smooth_3,ncol=pixels_smooth_3)
+  chm_smooth_3 = focal(chm, weights, fun=mean)
+
+  
+  
+  a = pmap(params %>% select(-method), vwf_singlechm_singleparamset , chm=chm, chm_smooth_1 = chm_smooth_1, chm_smooth_2 = chm_smooth_2, chm_smooth_3 = chm_smooth_3, layer_name = chm_layer_name)
+
+}
 
 
 
-    # ## error with either 14 or 39: fixed by installing from github
-# Error in .local(x, ...) : w must have uneven sides
-# In addition: There were 13 warnings (use warnings() to see them)
 
-#7,32
+### Define parameter values to search: only need to run if set defs change
+
+# b = seq(0.00, 0.06, by=0.01)
+# a = seq(0.0, 1, by = 0.1)
+# smooth = c(0,1,2,3)
+# 
+# params = expand.grid(a=a,b=b,smooth=smooth)
+# 
+# # remove sets where both a and b are zero
+# params = params %>%
+#   filter(!(a == 0 & b == 0))
+# 
+# params$detection_params_name = paste0("vwf_",str_pad(1:nrow(params), width=3, pad = "0"))
+# 
+# params$method = "vwf"
+# 
+# # save vwf params
+# write_csv(params,data("parameter_set_definitions/vwfdefs_fullrange.csv"))
+
+# load vwf params
+params = read_csv(data("parameter_set_definitions/vwfdefs_fullrange.csv"))
+
+
+### Define parameter values to search (simple version: only need to run if set defs change
+
+b = seq(0.05, 0.05, by=0.02)
+a = seq(0.6, 0.8, by = 0.2)
+smooth = c(0,2)
+
+params = expand.grid(a=a,b=b,smooth=smooth)
+
+# remove sets where both a and b are zero
+params = params %>%
+  filter(!(a == 0 & b == 0))
+
+params$detection_params_name = paste0("vwf_",str_pad(1:nrow(params)+500, width=3, pad = "0"))
+
+params$method = "vwf"
+
+# save vwf params
+write_csv(params,data("parameter_set_definitions/vwfdefs_simp.csv"))
+
+# load vwf params
+params = read_csv(data("parameter_set_definitions/vwfdefs_simp.csv"))
+
+
+
+
+
+# Run for multiple CHMs
+plan(multiprocess)
+
+paramset_names = c("paramset14_01","paramset15a_02")
+
+walk(paramset_names,.f = vwf_singlechm_multiparamset, params = params)
+
+
