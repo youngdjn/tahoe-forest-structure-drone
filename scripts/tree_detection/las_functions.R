b628c2c0256c83ade5b97544df69ae42c2394c1a
Derek <derek.j.n.young@gmail.com>
2020-05-12 13:12:46 -0700

    LAS-based tree detection scripts

---
+++ scripts/tree_detection/las_functions.R
@@ -0,0 +1,117 @@
+## Takes a CHM and makes a map of treetops
+
+
+## Convenience functions ####
+source(here("scripts/convenience_functions.R"))
+
+
+las_singlelas_allparams = function(las_layer_name, params) {
+
+  #### Load data ####
+  
+  # find the las file in the metashape products direcotry
+  las_files = list.files(data("metashape_products/las"),pattern=las_layer_name, full.names=TRUE)
+  if(length(las_files) > 1) {
+    
+    cat("More than 1 matching LAS file in the specified metashape data products folder for",las_layer_name,". Using most recent.\n") 
+    
+    # get the date
+    pieces = str_split(las_files,"/")
+    filenames = map(pieces,sapply(pieces,length)[1]) %>% unlist
+    dates = filenames %>% str_split("_") %>% map(3) %>% unlist
+    # which date is later?
+    latest = which(order(dates) == max(order(dates)))
+    las_file = las_files[latest]
+    
+  } else if(length(las_files) == 0) {
+      stop("No matching LAS files int he specified metashape data products folder.")
+  } else {
+    las_file = las_files
+  }
+  
+  las = readLAS(las_file)
+  
+  # temp for dev
+  #las = decimate_points(las, homogenize(10,1))
+  
+  ## Get the LAS layer name from the las_file
+  pieces = str_split(las_file,"/")
+  filename = map(pieces,sapply(pieces,length)[1]) %>% unlist
+  filename_noextension = str_split(filename,fixed(".")) %>% map(1) %>% unlist()
+  las_layer_name = filename_noextension
+  
+  
+  ### Run all the tree detection algorithms
+  
+  for(i in 1:nrow(params)) {
+    
+    params_current = params[i,]
+    
+    ### If the output already exists, skip
+    # First, what is the file?
+    file_name = paste0(las_layer_name,"-",params_current$detection_params_name,".geojson")
+    dir = data(paste0("detected_trees"))
+    path_file = paste0(dir,"/",file_name)
+    if(file.exists(path_file)) {
+      cat("Output already exists:",path_file,", skipping.\n")
+      next()
+    }
+    
+    
+    ### Thin the LAS as specified by the params
+    las_thinned = decimate_points(las, homogenize(params_current$decimate,1))
+
+    
+    if(params_current$method == "li") {
+      
+      segmented = segment_trees(las_thinned,li2012(hmin=5,dt1=params_current$dt1,dt2=params_current$dt2, Zu = params_current$zu, R=params_current$r, speed_up = params_current$speedUp))
+      
+      ttops_sf = segmented %>% 
+        slot("data") %>% 
+        dplyr::group_by(treeID) %>%
+        dplyr::filter(Z == max(Z)) %>%
+        dplyr::ungroup() %>%
+        st_as_sf(coords = c("X", "Y"),
+                 crs = proj4string(las))
+
+    } else if(params_current$method == "lmfx") {
+      
+      ttops = find_trees(las_thinned, lmfx(hmin=5,dist_2d=params_current$dist2d,ws=params_current$ws))
+      
+      ttops_sf = ttops %>%
+        st_as_sf()
+
+    } else if(params_current$method == "multichm") {
+      
+      ttops = find_trees(las_thinned, multichm(hmin=5,dist_2d=params_current$dist2d,ws=params_current$ws, res = params_current$res, layer_thickness = params_current$layer_thickness, dist_3d = params_current$dist3d, use_max = params_current$use_max))
+
+      ttops_sf = ttops %>%
+        st_as_sf()
+
+    } else if(params_current$method == "lmfauto") {
+      
+      ttops = find_trees(las_thinned, lmfauto(hmin=5))
+      
+      ttops_sf = ttops %>%
+        st_as_sf()
+      
+    }
+    
+    ttops_sf = ttops_sf %>%
+      rename(height = Z) %>%
+      st_set_agr("constant") %>%
+      dplyr::select(height) %>%
+      st_transform(4326)
+    
+    ## make file name to save, and save
+    dir.create(dir)
+    st_write(ttops_sf,path_file, delete_dsn=TRUE, quiet=TRUE)
+
+  }
+  
+  return(TRUE)
+  
+}
+
+
+
---
+++ scripts/tree_detection/las_run_comparison.R
@@ -0,0 +1,70 @@
+## Takes a las and makes a map of treetops following different methods
+
+library(sf)
+library(lidR)
+library(lidRplugins)
+library(here)
+library(purrr)
+library(furrr)
+library(tidyverse)
+
+#### Get data dir ####
+# The root of the data directory
+data_dir = readLines(here("data_dir.txt"), n=1)
+
+#### Convenience functions and main functions ####
+
+source(here("scripts/convenience_functions.R"))
+source(here("scripts/tree_detection/las_functions.R"))
+
+
+
+
+### Define parameter values to search: only need to run if set defs change
+
+params = read_csv(data("parameter_set_definitions/best_las.csv"))
+params$detection_params_name = paste0("las_",str_pad(1:nrow(params)+9000, width=4, pad = "0"))
+params$method_general = "las"
+
+
+
+# If running manually, specify paramset names
+manual_paramset_names = NULL
+# manual_paramset_names = c("paramset14b_04","paramset14_02","paramset14_03")
+
+# read paramset from command line argument (otherwise use the hard-coded default above)
+command_args = commandArgs(trailingOnly=TRUE)
+
+if(length(command_args) == 0) {
+  if(!is.null(manual_paramset_names)) {
+    paramset_names = manual_paramset_names
+  } else { # pull from directory
+    
+    las_files = list.files(data("metashape_products/las"),pattern="points\\.laz", full.names=TRUE)
+    
+    # get all the filenames from before the date
+    pieces = str_split(las_files,"/")
+    filenames = map(pieces,sapply(pieces,length)[1]) %>% unlist
+    pre_dates_part1 = filenames %>% str_split("_") %>% map(1) %>% unlist
+    pre_dates_part2 = filenames %>% str_split("_") %>% map(2) %>% unlist
+    pre_dates = paste(pre_dates_part1,pre_dates_part2,sep="_")
+    
+    
+    paramset_names = unique(pre_dates)
+    
+  }
+  
+} else if (length(command_args) > 0) {
+  paramset_names = command_args[1]
+}
+
+# Ramdomize paramset names so can run multiple parallel
+paramset_names = paramset_names %>% sample()
+
+### Run the search
+set_lidr_threads(1)
+options(future.globals.maxSize=5000*1024^2) # 5 GB
+plan(multiprocess)
+
+
+future_map(paramset_names,.f = las_singlelas_allparams, params = params, .options=future_options(scheduling=Inf))
