9eee5a79a945604205f3a72d209a03b1afd2c492
Derek <derek.j.n.young@gmail.com>
2021-01-22 11:53:42 -0800

    Make even coarser CHM

--- scripts/downscale_chm.R
+++ scripts/downscale_chm.R
@@ -34,6 +34,10 @@ names_df = names_df %>%
 tgt_res = c(0.03, 0.06, 0.12) # names for these should be 3xxx, 2xxx, 1xxx
 tgt_name = c(3, 2, 1)
 
+# New:
+tgt_res = c(0.25)
+tgt_name = c(4)
+
 ### Downscale each raster to each resolution
 for(i in 1:nrow(names_df)) {
   names_row = names_df[i,]
@@ -44,7 +48,7 @@ for(i in 1:nrow(names_df)) {
     # What will the downscaled raster be named?
     metashape_set_name = paste0(tgt_name[j],names_row$metashape_parameters)
     filename = paste(names_row$photoset, metashape_set_name, names_row$timestamp, "dsm_chm.tif",sep = "_")
-    path_filename = data(paste0("metashape_outputs_postprocessed/chm/",filename))
+    path_filename = data(paste0("metashape_outputs_postprocessed/chm_new/",filename))
     
     # Does it exist already? If so, skip
     if(file.exists(path_filename)) {
