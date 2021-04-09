c47b42d13ed968d65660d6ef47b8578aa56c5e2c
Derek <derek.j.n.young@gmail.com>
2021-04-09 15:49:31 -0700

    Fix directory for Metashape 1.7 run

--- scripts/crop_and_normalize_dsm.R
+++ scripts/crop_and_normalize_dsm.R
@@ -28,7 +28,7 @@ dtm = raster(data("dem_usgs/dem_usgs.tif")) %>% projectRaster(crs = "+proj=utm +
 
 
 ## get DSM layers from metashape outputs directory
-dsm_files = list.files("/storage/forestuav/metashape_outputs/comparison_project_exposure_and_composite",pattern=".*_dsm\\.tif", full.names=TRUE)  # to filter to ones matching a name: pattern=paste0(las_layer_name,".*\\.las")
+dsm_files = list.files("/storage/forestuav/meta17/metashape_outputs",pattern=".*_dsm\\.tif", full.names=TRUE)  # to filter to ones matching a name: pattern=paste0(las_layer_name,".*\\.las")
 
 
 crop_and_write_chm = function(dsm_file) {
